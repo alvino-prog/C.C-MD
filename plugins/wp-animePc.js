ba8ff3246c8c3cc89b6262905bf6c2387d78b4ab
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-06 18:53:05 +0000

    Create wp-animePc.js

---
+++ plugins/wp-animePc.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn }) => {
+    try {
+        let res = await fetch(global.API('xteam', '/randomimage/wallpaper', {}, 'APIKEY'))
+        if (res.status != 200) throw await res.text()
+        let img = await res.buffer()
+        conn.sendFile(m.chat, img, '', '*© C.C-Bot*', m, false, { thumbnail: Buffer.alloc(0) })
+    } catch (e) {
+        throw `Limit apikey habis atau error!`
+    }
+}
+handler.help = ['wallpaperanime2']
+handler.tags = ['internet']
+handler.command = /^(wallpaper2|wp2)anime$/i
+handler.limit = true
+
+module.exports = handler
