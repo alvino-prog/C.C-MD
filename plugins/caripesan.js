83d8566d8d0ad527f642d48c8bed4f540d22dff3
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-06 17:39:01 +0000

    Update caripesan.js

--- plugins/caripesan.js
+++ plugins/caripesan.js
@@ -18,5 +18,6 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['caripesan <pesan>|<jumlah>']
 handler.tags = ['tools']
 
-handler.command = /^(caripesan)$/i
+handler.command = /^caripesan/i
+
 module.exports = handler
