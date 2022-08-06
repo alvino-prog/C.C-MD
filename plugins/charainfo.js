b0ce39ab0a2dd94398d2f67c9e2e1ec963e3017e
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-06 16:25:59 +0000

    Update charainfo.js

--- plugins/charainfo.js
+++ plugins/charainfo.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
   if (!text) throw `Masukkan query!`
-  let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/character', { q: text }))
+  let res = await fetch(global.API('https://api.jikan.moe', '/v4/characters', { q: text }))
   if (!res.ok) throw await res.text()
-  let json = await res.json()
-  let { name, alternative_names, url, image_url, type } = json.results[0]
+  let json = await res.json().console.log(json)
+  let { name, nicknames, url, image_url, about, type } = json.results[0]
 let charaingfo = `💬 *Name:* ${name}
-💭 *Nickname:* ${alternative_names}
+💭 *Nickname:* ${nicknames}
 🔗 *Link*: ${url}
-👤 *Character Type*: ${type}`
+👤 *About*: ${about}`
 
   conn.sendFile(m.chat, image_url, '', charaingfo, m)
 }
@@ -17,4 +17,5 @@ handler.tags = ['internet']
 handler.command = /^(chara|character)$/i
 //kyaa jangan biarkan wabot-aq terbengkalai sampai nurutomo kembali
 // Command - Re Edited -- TOXIC-DEVIL == || Character Type ||
+
 module.exports = handler
