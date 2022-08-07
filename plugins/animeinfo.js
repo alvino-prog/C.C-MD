6938cf2c5ca905daf369a5e1fb8b33b79659d171
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-07 08:37:54 +0000

    Update animeinfo.js

--- plugins/animeinfo.js
+++ plugins/animeinfo.js
@@ -6,19 +6,19 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   console.log(json)
-  let { title, members, synopsis, episodes, url, rating, score, image_url, status, type, aired, from, to, duration, genres, mal_id } = json.data[0]
+  let { title, members, synopsis, episodes, url, rating, score, image_url, status, type, duration, mal_id } = json.data[0]
   //Scrape Genre MAL by DwiR
  /*let res2 = await fetch(`https://myanimelist.net/anime/${mal_id}`)
   if (!res2.ok) throw await res2.text()
   let html = await res2.text()
   let { document } = new JSDOM(html).window
-  let genAnim = [...document.querySelectorAll('div[class="spaceit_pad"] > * a')].map(el => el.href).filter(href => href.startsWith('/genres/anime/'))*/
+  let genAnim = [...document.querySelectorAll('div[class="spaceit_pad"] > * a')].map(el => el.href).filter(href => href.startsWith('/anime/genre/'))*/
   let animeingfo = `✨️ *Title:* ${title}
 🎆️ *Episodes:* ${episodes}
-🎗️ *Genre:* ${genres}
+
 💫 *Status:* ${status}
 ⏳ *Duration:* ${duration}
-➡️ *On air:* ${aired}
+
 💬 *Show Type:* ${type}
 💌️ *Rating:* ${rating}
 ❤️ *Score:* ${score}
@@ -30,5 +30,5 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['anime <judul>']
 handler.tags = ['internet']
 handler.command = /^(anime|animeinfo)$/i
-//maapin fatur :<
+
 module.exports = handler
