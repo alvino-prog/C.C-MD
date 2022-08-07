8b03feae933e9acca377eb54015334c8edba13c9
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-07 10:54:59 +0000

    Create nsfw-waifu.js

---
+++ plugins/nsfw-waifu.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/nsfw/waifu')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Panas panas Sange euy', m)
+}
+handler.help = ['waifus']
+handler.tags = ['internet']
+handler.command = /^(waifus)$/i
+
+module.exports = handler
