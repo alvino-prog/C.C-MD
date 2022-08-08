3a02db1bf94f97eb8a168425817605b41fa63dab
Alvinocaharles <54900965+alvino-prog@users.noreply.github.com>
2022-08-08 09:01:27 +0000

    Update charainfo.js

--- plugins/charainfo.js
+++ plugins/charainfo.js
@@ -5,13 +5,13 @@ let handler = async(m, { conn, text }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   console.log(json)
-  let { name, nicknames, name_kanji, url, image, about, type } = json.data[0]
+  let { name, nicknames, name_kanji, url, images, about, type } = json.data[0]
 let charaingfo = `💬 *Name:* ${name} ${name_kanji}
 💭 *Nickname:* ${nicknames}
 🔗 *Link*: ${url}
 👤 *About*: ${about}`
 
-  conn.sendFile(m.chat, image, '', charaingfo, m)
+  conn.sendFile(m.chat, images, '', charaingfo, m)
 }
 handler.help = ['character <nama>']
 handler.tags = ['internet']
